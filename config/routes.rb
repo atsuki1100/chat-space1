f29f87d887b090d466b42a0960dc17966df59136
atsuki1100 <atuki0303@outlook.jp>
2019-06-23 12:54:00 +0900

    わからない。グループ編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
+  root 'groups#index'
   resources :users, onry: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, onry: [:index, :create]
