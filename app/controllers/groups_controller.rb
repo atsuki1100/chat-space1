7a36e182d7fa65f47b5130ca703ad9521eb20ce7
atsuki1100 <atuki0303@outlook.jp>
2019-06-23 15:55:50 +0900

    View Update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,4 @@
 class GroupsController < ApplicationController
-  before_action :det_group, onry: [:edit, :update]
-
   def index
   end
 
@@ -27,11 +25,12 @@ class GroupsController < ApplicationController
   end
 
   private
+
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
   end
 
-  def det_group
+  def set_group
     @group = Group.find(params[:id])
   end
 end
