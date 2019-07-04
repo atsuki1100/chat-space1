c201871f734421e14f0c249c9d752316cfbe7ed6
atsuki1100 <atuki0303@outlook.jp>
2019-07-04 13:24:48 +0900

    update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,13 @@ set :linked_files, %w{ config/secrets.yml }
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+
 set :ssh_options, auth_methods: ['publickey'],
                   keys: '~/.ssh/11000303.pem'
 
